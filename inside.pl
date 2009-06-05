060d1880f57a6d8c023c268fb6e3eb7d4234ec26
Christian Theil Have <christiantheilhave@gmail.com>
2009-06-05 11:53:51 +0200

    Inside done. Confluent?

--- inside.pl
+++ inside.pl
@@ -1,12 +1,31 @@
-:- chr_constraint rule/3, sentence/1, word/2.
-%test
+:- use_module(library(chr)).
+
+:- chr_constraint rule/3, sentence/1, sentence/2, word/2, beta/4.
+
+sentence(S) <=> sentence(1,S).
 remove_empty_sentence @ sentence(_,[]) <=> true.
 split_sentence @
-sentence(Time,[Elem|R]) <=> 
-    NextTime is Time + 1 | input(Time,Elem), sentence(NextTime,R).
+sentence(Index,[Word|R]) <=> 
+    NextIndex is Index + 1 | word(Index,Word), sentence(NextIndex,R).
 
-%rule(np, [np,vp], 0.2)
+sum_multiple @
+beta(N,P,Q,Prob1), beta(N,P,Q,Prob2) <=>
+    Prob is Prob1 + Prob2
+    |
+    beta(N,P,Q,Prob).
 
-% beta(J, From, To)
+base_case @
+word(Pos,Word), rule(NonTerm,[Word],Prob) ==> beta(NonTerm, Pos, Pos, Prob).
 
+recursion @
+rule(NonTermJ, [NonTermR,NonTermS], JProb),
+beta(NonTermR, P, D, ProbR), beta(NonTermS, D1, Q, ProbS) ==>
+   BetaProb is ProbR * ProbS * JProb
+   |
+   beta(NonTermJ, P, Q, BetaProb).
+
+init_grammar :- 
+    rule(np, [det,noun],1.0),
+    rule(det, [the], 1.0),
+    rule(noun, [boy], 1.0).
 
