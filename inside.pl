57ecf0b6e881b2843fbec4ddd110fa9033e0891e
Christian Theil Have <christiantheilhave@gmail.com>
2009-06-21 18:01:57 +0200

    fixing outside

--- inside.pl
+++ inside.pl
@@ -1,5 +1,5 @@
 :- use_module(library(chr)).
-:- chr_constraint rule/3, sentence/1, sentence/2, sentence_length/1, word/2, inside/5, level/1, root/1, notroot/1, outside/4.
+:- chr_constraint rule/3, sentence/1, sentence/2, sentence_length/1, word/2, inside/5, level/1, root/1, notroot/1, outside/4, inside_done/0.
 %:- set_prolog_flag(chr_toplevel_show_store,false).
 
 :- [grammar].
@@ -35,7 +35,7 @@ inside(N,L1,P,Q,Prob1), inside(N,L2,P,Q,Prob2) <=>
 inside_base_case @
 level(1), % Not strictly necessary
 word(Pos,Word), rule(NonTerm,[Word],Prob) ==>
-	write('inside (basecase):'),nl, write(word(Pos,Word)), write(','),write(rule(NonTerm,[Word],Prob)), write('==>'), write(inside(NonTerm,1,Pos,Pos,Prob)), nl.
+	write('inside (basecase):'),nl, write(word(Pos,Word)), write(','),write(rule(NonTerm,[Word],Prob)), write('==>'), write(inside(NonTerm,1,Pos,Pos,Prob)), nl,
     inside(NonTerm, 1, Pos, Pos, Prob).
 
 inside_recursion @
@@ -53,25 +53,36 @@ level(L), rule(NonTermJ, [NonTermR,NonTermS], JProb), inside(NonTermR, L1, P, D,
     |
     inside(NonTermJ, NextLevel, P, Q, BetaProb).
 
-sentence_length(SL) \ level(L) <=> L < SL + 1 | write('increase level'), nl, M is L + 1, level(M).
+sentence_length(SL) \ level(L) <=>
+    L < SL,
+    M is L + 1    
+    |
+    write('increase level: '), write(M), nl,
+    level(M).
+
+sentence_length(L), level(L) ==> inside_done.
+
+
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % outside algorithm
 
 outside_base_case1 @
-sentence_length(L), rule(N,_,_), root(N) ==> outside(N,1,L,1).
+inside_done, sentence_length(L), rule(N,_,_), root(N) ==> outside(N,1,L,1).
 
 outside_base_case2 @
-sentence_length(L), rule(N,_,_), notroot(N) ==> outside(N,1,L,0).
+inside_done, sentence_length(L), rule(N,_,_), notroot(N) ==> outside(N,1,L,0).
 
 
 outside_sum_multiple @
-outside(N,P,Q,P1), outside(N,P,Q,P2) <=> 
+inside_done \ 
+outside(N,P,Q,P1), outside(N,P,Q,P2) <=>
 	Prob is P1 + P2,
 	write('outside_sum_multiple: '), write(outside(N,P,Q,P1)), write(','), write(outside(N,P,Q,P2)), write(' <=> '), write(outside(N,P,Q,Prob)),nl,
 	outside(N,P,Q,Prob).
 
 outside_left_recursion @
+inside_done,
 rule(NF, [NJ, NG],  PF), outside(NF,P,E,PaF), inside(NG, _Lvl, Q1, E, PbG) ==>
 	Q is Q1 - 1,
 	PJ is PF * PaF * PbG
@@ -80,13 +91,14 @@ rule(NF, [NJ, NG],  PF), outside(NF,P,E,PaF), inside(NG, _Lvl, Q1, E, PbG) ==>
 	outside(NJ,P,Q,PJ).
 	
 outside_right_recursion @
-rule(NF,[NJ,NG],PF), outside(NF,P,E,PaF), inside(NG,_Lvl,P,Q,PbG) ==>
+inside_done,
+rule(NF,[NJ,NG],PF), outside(NF,P,E,PaF), inside(NJ,_Lvl,P,Q,PbJ) ==>
 	Q1 is Q + 1,
-	PJ is PF * PaF * PbG
+	PJ is PF * PaF * PbJ
 	|
-	write('outside_right_rec: '), write(rule(NF,[NJ,NG],PF)), write(','), write(outside(NF,P,E,PaF)), write(','), write(inside(NG,_,P,Q,PbG)), write('==>'), write(outside(NJ,Q1,E,PJ)), nl,
-	outside(NJ,Q1,E,PJ).
-	
+	write('outside_right_rec: '), write(rule(NF,[NJ,NG],PF)), write(','), write(outside(NF,P,E,PaF)), write(','), write(inside(NJ,_,P,Q,PbJ)), write('==>'), write(outside(NG,Q1,E,PJ)), nl,
+	outside(NG,Q1,E,PJ).
+
 %% Prolog helpers:
 
 max(A,B,A) :- B =< A.
@@ -94,5 +106,7 @@ max(A,B,B) :- A < B.
 
 test :-
 	init_grammar,
-	sentence([astronomers, saw, stars, with, ears]),
+%	sentence([astronomers, saw, stars, with, ears]),
+	sentence([astronomers,saw,stars]),
+%	sentence([stars]),
 	level(1).
